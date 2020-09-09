e6637d86736b6ac9f98aceb385debfe22321a480
markewilson <mark.e.wilson@gmail.com>
2020-09-09 14:51:03 -0400

    adds config for console and adds texas_asa24 custom module

---
+++ web/modules/custom/texas_asa24/src/Controller/TexasAsa24Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\texas_asa24\Controller;
+use Drupal\Core\Controller\ControllerBase;
+/**
+ *In case of no default template of module, below markup will be displayed
+ */
+class TexasAsa24Controller extends ControllerBase {
+  public function content() {
+    $user = 6009;
+    return array('#markup' => texas_asa24_generate_jwt($user));
+  }
+
+}
