ee3d7c168367b5936d4837b530d56d333cbe4b1d
markewilson <mark.e.wilson@gmail.com>
2020-09-09 15:25:00 -0400

    adds more ASA config strings to settings array

--- web/modules/custom/texas_asa24/src/Form/Asa24SettingsForm.php
+++ web/modules/custom/texas_asa24/src/Form/Asa24SettingsForm.php
@@ -20,12 +20,36 @@ class Asa24SettingsForm extends ConfigFormBase {
 
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $config = $this->config('texas_asa24.credentials');  //since we are extending ConfigFormBase instaad of FormBase, it gives use access to the config object
+    $config = $this->config('texas_asa24.credentials');  //since we are extending ConfigFormBase instead of FormBase, it gives use access to the config object
     $form['api_token'] = array(
       '#type' => 'textfield',
       '#description' => t('API Token'),
       '#title' => t('API Token'),
       '#default_value' => $config->get('api_token'),
+    );
+    $form['base_url'] = array(
+      '#type' => 'textfield',
+      '#description' => t('ASA24 Base URL'),
+      '#title' => t('Base URL'),
+      '#default_value' => $config->get('base_url'),
+    );
+    $form['user_prefix'] = array(
+      '#type' => 'textfield',
+      '#description' => t('Prefix for ASA24 unique user, to be prepended to pptid'),
+      '#title' => t('User Prefix'),
+      '#default_value' => $config->get('user_prefix'),
+    );
+    $form['redirect_url'] = array(
+      '#type' => 'textfield',
+      '#description' => t('Base URL for ASA24 redirect'),
+      '#title' => t('Redirect URL'),
+      '#default_value' => $config->get('redirect_url'),
+    );
+    $form['token_life'] = array(
+      '#type' => 'textfield',
+      '#description' => t('How long a JWT token lasts e.g. +3 month'),
+      '#title' => t('JWT Token Life'),
+      '#default_value' => $config->get('token_life'),
     );
     return parent::buildForm($form,$form_state);
   }
@@ -40,6 +64,10 @@ class Asa24SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('texas_asa24.credentials')
       ->set('api_token', $form_state->getValue('api_token'))
+      ->set('base_url', $form_state->getValue('base_url'))
+      ->set('token_life', $form_state->getValue('token_life'))
+      ->set('redirect_url', $form_state->getValue('redirect_url'))
+      ->set('user_prefix', $form_state->getValue('user_prefix'))
       ->save();
   }
 }
