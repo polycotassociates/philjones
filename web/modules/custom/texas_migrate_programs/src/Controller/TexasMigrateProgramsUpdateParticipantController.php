782dc537f8013d28f1b6a504d28e6198db9438ba
markewilson <mark.e.wilson@gmail.com>
2021-01-31 17:55:10 -0500

    adds controller for texas_migrate_programs/update_participant/ and tewaks functions in texas_migrate_programs.module to allow more granularity in imports

---
+++ web/modules/custom/texas_migrate_programs/src/Controller/TexasMigrateProgramsUpdateParticipantController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\texas_migrate_programs\Controller;
+use Drupal\Core\Controller\ControllerBase;
+/**
+ *In case of no default template of module, below markup will be displayed
+ */
+class TexasMigrateProgramsUpdateParticipantController extends ControllerBase {
+  public function content($responseids,$program) {
+    return array('#markup' => texas_migrate_programs_import_responseid($responseids,$program));
+  }
+
+}
