6490642443223dbbfe6522cf08338e8f3c8e7de2
markewilson <mark.e.wilson@gmail.com>
2021-02-01 17:38:39 -0500

    strips out querystring from Qualtrics survey and measures terminations to require only pptid in string. now hard-coded for baseline

--- web/modules/custom/texas_qualtrics_update/src/Controller/TexasMeasuresCompleteController.php
+++ web/modules/custom/texas_qualtrics_update/src/Controller/TexasMeasuresCompleteController.php
@@ -3,80 +3,54 @@
 namespace Drupal\texas_qualtrics_update\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\user\Entity\User;
 /**
  *In case of no default template of module, below markup will be displayed
  */
 class TexasMeasuresCompleteController extends ControllerBase {
   public function content($pptstring) {
     $markup = '';
-    $timepointid = '';
-    // programs that have field_measures_completed
-    $measureprogs = array('dep_l', 'dep_f', 'htds_f', 'htds_l', 'susd_f');
-    // break string into pptid and timepoints
-    $splits = explode('&', $pptstring);
-    $pptid = ltrim(strstr(array_shift($splits), '='), '=');
+    $timepointid = 30;
+    // get pptid out of string
+    $pptid = ltrim(strstr($pptstring, '='), '=');
     // get user id from pptid and add to link
     $uids = \Drupal::entityQuery('user')
       ->condition('field_response_id', $pptid)
       ->execute();
     foreach($uids as $uid)
       {
-        $uid = $uid;
+        $user = User::load($uid);
+        $username = $user->get('field_displa')->value;
       }
-    foreach ($splits as $split) {
-      $program = str_replace('-', '_',explode('=', $split, 2));
-      $program = strtolower($program[0]);
-      $timepoint = ltrim(strstr($split, '='), '=');
-      // map to timepoint term id
-      // check to see if it's a program with asa24
-      foreach ($measureprogs as $p ) {
-      if (strpos ($program , $p ) !== FALSE ) {
-        // append rest of paragraph id
-        $paragraphid = $program .'_timepoint';
-        if(strpos($timepoint, 'N') !== true) {
-          if ($timepoint == 'B'){
-            $timepointid = 30;
-          }
-        if ($timepoint == '12W'){
-            $timepointid = 74;
-          }
-        if ($timepoint == '24W'){
-            $timepointid = 75;
-          }
-        if ($timepoint == '36W'){
-            $timepointid = 76;
-          }
-        if ($timepoint == '48W'){
-            $timepointid = 77;
-          }
       // get paragraph ids filtered by pptid and timepoint
       $pids = \Drupal::entityQuery('paragraph')
-        ->condition('type', $paragraphid)
+        //->condition('type', 'dep_f_timepoint')
         ->condition('field_participant_id', $pptid)
         ->condition('field_timepoint', $timepointid)
+        //->condition('field_start_date.value', time(), '<=')
+        ->condition('field_measures_completed', 0)
         ->execute();
-
         // loop through paragraphs and update
         foreach($pids as $pid) {
           $paragraph = Paragraph::load($pid);
+          //var_dump(strtotime($paragraph->get('field_start_date')->value));
+          //var_dump(time());
           $paragraph_field_value = $paragraph->get('field_measures_completed')->value;
-          // var_dump($paragraph_field_value);
           // Update field_measures_completed to true.
           $paragraph->set('field_measures_completed', 1);
           // Save the Paragraph.
           $paragraph->save();
           //display debug info
-          //$debug = $debug . '<li>Program: '. $program.'</li><li>Paragraph: '. $paragraphid.'</li><li>Timepoint: '. $timepoint.'</li><li>Timepoint ID: '. $timepointid.'</li><li>Paragraph ID '.$pid .'</li><li>field_measures_completed Value '.$paragraph_field_value.'</li>';
+          //$debug = $debug . '<ul><li>Paragraph: '. $pid.'</li><li>Timepoint ID: '. $timepointid.'</li><li>Paragraph ID: '.$pid .'</li><li>field_measures_completed Value: '.$paragraph_field_value.'</li></ul>';
 
 
-          }
-        }
-      }
+
+
     }
-  }
 
-    //$markup = '<ul><li>Participant ID: '.$pptid .'</li><li>User ID: '.$uid .'</li><ul>' .$debug. '</ul></ul>';
-    $markup = $markup . '<p>The data collection visit has been recorded in the participant’s record</p>';
+
+    //$markup = '<ul><li>Participant ID: '.$pptid .'</li><li>User ID: '.$uid .'</li>' .$debug. '</ul>';
+    $markup = $markup . '<p>The data collection visit has been recorded for participant <strong>'.$username.'</strong>.</p>';
     $markup = $markup . '<a href="/user/'.$uid .'" class="btn btn-info" role="button">Return to this participant\'s dashboard</a>';
     return array('#markup' => $markup );
   }
--- web/modules/custom/texas_qualtrics_update/src/Controller/TexasSurveyCompleteController.php
+++ web/modules/custom/texas_qualtrics_update/src/Controller/TexasSurveyCompleteController.php
@@ -3,77 +3,47 @@
 namespace Drupal\texas_qualtrics_update\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\user\Entity\User;
 /**
  *In case of no default template of module, below markup will be displayed
  */
 class TexasSurveyCompleteController extends ControllerBase {
   public function content($pptstring) {
     $markup = '';
-    $timepointid = '';
-    // programs that have field_survey_completed
-    $surveyprogs = array('cwwd', 'dep_f', 'htds_f', 'map', 'susd_f', 'susd_l', 'wat');
-    // break string into pptid and timepoints
-    $splits = explode('&', $pptstring);
-    $pptid = ltrim(strstr(array_shift($splits), '='), '=');
+    $timepointid = 30;
+    // get pptid out of string
+    $pptid = ltrim(strstr($pptstring, '='), '=');
     // get user id from pptid and add to link
     $uids = \Drupal::entityQuery('user')
       ->condition('field_response_id', $pptid)
       ->execute();
     foreach($uids as $uid)
       {
-        $uid = $uid;
+        $user = User::load($uid);
+        $username = $user->get('field_displa')->value;
       }
-    foreach ($splits as $split) {
-      $program = str_replace('-', '_',explode('=', $split, 2));
-      $program = strtolower($program[0]);
-      $timepoint = ltrim(strstr($split, '='), '=');
-      // map to timepoint term id
-      // check to see if it's a program with asa24
-      foreach ($surveyprogs as $p ) {
-      if (strpos ($program , $p ) !== FALSE ) {
-        // append rest of paragraph id
-        $paragraphid = $program .'_timepoint';
-        if(strpos($timepoint, 'N') !== true) {
-          if ($timepoint == 'B'){
-            $timepointid = 30;
-          }
-        if ($timepoint == '12W'){
-            $timepointid = 74;
-          }
-        if ($timepoint == '24W'){
-            $timepointid = 75;
-          }
-        if ($timepoint == '36W'){
-            $timepointid = 76;
-          }
-        if ($timepoint == '48W'){
-            $timepointid = 77;
-          }
+
       // get paragraph ids filtered by pptid and timepoint
       $pids = \Drupal::entityQuery('paragraph')
-        ->condition('type', $paragraphid)
+        //->condition('type', $paragraphid)
         ->condition('field_participant_id', $pptid)
         ->condition('field_timepoint', $timepointid)
+        ->condition('field_survey_completed', 0)
         ->execute();
         // loop through paragraphs and update
         foreach($pids as $pid) {
           $paragraph = Paragraph::load($pid);
           $paragraph_field_value = $paragraph->get('field_survey_completed')->value;
-          // var_dump($paragraph_field_value);
+           var_dump($paragraph_field_value);
           // Update field_survey_complete to true.
           $paragraph->set('field_survey_completed', 1);
           // Save the Paragraph.
           $paragraph->save();
-          //$debug = $debug . '<li>Program: '. $program.'</li><li>Paragraph: '. $paragraphid.'</li><li>Timepoint: '. $timepoint.'</li><li>Timepoint ID: '. $timepointid.'</li><li>Paragraph ID '.$pid .'</li><li>field_survey_completed Value '.$paragraph_field_value.'</li>';
-
+          //$debug = $debug . '<ul><li>Timepoint ID: '. $timepointid.'</li><li>Paragraph ID: '.$pid .'</li><li>field_survey_completed value was: '.$paragraph_field_value.'</li></ul>';
         }
-        }
-      }
-    }
-  }
 
-    //$markup = '<ul><li>Participant ID: '.$pptid .'</li><li>User ID: '.$uid .'</li><ul>' .$debug. '</ul></ul>';
-    $markup = $markup . '<p>Thank you for completing your online Health Survey, your record has now been updated</p>';
+    //$markup = '<ul><li>Participant ID: '.$pptid .'</li><li>User ID: '.$uid .'</li>' .$debug. '</ul>';
+    $markup = $markup . '<p>Thank you for completing the online Health Survey, participant <strong>'.$username.'</strong> has now been updated.</p>';
     $markup = $markup . '<a href="/user/'.$uid .'" class="btn btn-info" role="button">Return to this participant\'s dashboard</a>';
     return array('#markup' => $markup );
   }
